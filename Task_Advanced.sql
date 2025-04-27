4cf099acfcce71cc40aa3c5530a27c8c45cad00a
Randxm4est <avirukth@gmail.com>
2025-04-27 12:58:45 +1000

    Stored Procedure

--- Task_Advanced.sql
+++ Task_Advanced.sql
@@ -30,3 +30,49 @@ where return_date is NULL
 and (current_date-ist.issued_date)>30
 order by
 5 desc
+
+--  Update  status of books in the books table to "Yes" when they are returned (based on entries in the return_status table).
+
+--Manual Approach-- NOT IDEAL
+
+select * from books 
+where isbn='978-0-451-52994-2'
+
+select * from issued_status
+where issued_book_isbn='978-0-451-52994-2'
+
+select * from return_status where issued_id= 'IS130'
+
+update books set status = 'no' where isbn ='978-0-451-52994-2'
+
+--Store Procedures
+CREATE OR REPLACE PROCEDURE add_return_records(p_return_id varchar(10),p_issued_id varchar(20),p_book_quality varchar(20))
+language plpgsql
+as $$
+
+
+declare
+	v_isbn varchar(50);
+	v_book_name varchar(60);
+begin
+	--Inserting into return_status
+	insert into return_status(return_id,issued_id,return_date,book_quality) 
+	values 
+	(p_return_id,p_issued_id,current_date,p_book_quality);
+
+	select issued_book_isbn,issued_book_name into v_isbn,v_book_name 
+	from issued_status
+	where issued_id=p_issued_id;
+
+	
+
+	update books 
+	set status = 'yes' 
+	where isbn =   v_isbn;
+
+	raise notice 'Thank You for Returning the Book: %',v_book_name;
+
+end
+$$
+
+call add_return_records('RS138','IS135','Good');
